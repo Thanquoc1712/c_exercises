db7592482f6742f71cd8ac7d461a15f30373882b
Thanquoc1712 <93112689+Thanquoc1712@users.noreply.github.com>
2023-04-05 21:01:44 +0700

    Add files via upload

---
+++ Embedded/timer_delay.ino
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <esp32-hal-timer.h>
+
+hw_timer_t *timer = NULL; // Khai báo biến timer
+
+void IRAM_ATTR onTimer() {
+  static bool ledState = false;
+  digitalWrite(2, ledState); // Điều khiển đèn LED trên chân GPIO2
+  ledState = !ledState;
+    Serial.println("Timer Interrupt");
+
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(2, OUTPUT); // Khai báo  GPIO2 điều khiển đèn LED
+
+  timer = timerBegin(0, 80, true); // Tạo một timer với số thứ tự 0, chia tỷ lệ tần số 80
+  timerAttachInterrupt(timer, &onTimer, true); // Gắn hàm ngắt với timer
+  timerAlarmWrite(timer, 2000000, true); // Thiết lập thời gian delay 1 giây
+  timerAlarmEnable(timer); // Bật timer
+}
+
+void loop() {
+}
